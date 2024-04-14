d453398b2469130f9c7a1833f63b9d4fbfd87514
souvikree <souvikg3225@gmail.com>
2024-04-14 16:39:37 +0530

    focus, playlists, sound of india

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -4,6 +4,61 @@ import IconText from "../components/shared/IconText";
 import TextHover from "../components/shared/TextHover";
 import { Link } from "react-router-dom";
 
+const focusCardsData= [
+  {
+      title: "Peaceful Piano",
+      description: "Relax and indulge with beautiful piano pieces",
+      imgUrl: "https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1546&q=80",
+  },
+  {
+      title: "Deep Focus",
+      description: "Keep calm and focus with this music",
+      imgUrl: "https://images.unsplash.com/photo-1558021212-51b6ecfa0db9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1766&q=80",
+  },
+  {
+      title: "Instrumental Study",
+      description: "Focus with soft study music in the background.",
+      imgUrl: "https://images.unsplash.com/photo-1612225330812-01a9c6b355ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+  },
+  {
+      title: "Focus Flow",
+      description: "Up tempo instrumental hip hop beats",
+      imgUrl: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  },
+  {
+      title: "Beats to think to",
+      description: "Focus with deep techno and tech house",
+      imgUrl: "https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  },
+];
+const spotifyPlaylistsCardData = [
+  {
+      title: "This is one",
+      description: "Relax and indulge with beautiful piano pieces",
+      imgUrl: "https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1546&q=80",
+  },
+  {
+      title: "Deep Focus",
+      description: "Keep calm and focus with this music",
+      imgUrl: "https://images.unsplash.com/photo-1558021212-51b6ecfa0db9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1766&q=80",
+  },
+  {
+      title: "Instrumental Study",
+      description: "Focus with soft study music in the background.",
+      imgUrl: "https://images.unsplash.com/photo-1612225330812-01a9c6b355ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+  },
+  {
+      title: "Focus Flow",
+      description: "Up tempo instrumental hip hop beats",
+      imgUrl: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  },
+  {
+      title: "Beats to think to",
+      description: "Focus with deep techno and tech house",
+      imgUrl: "https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  },
+];
+
 const HomeComponent = () => {
   return (
     <div className="h-full w-full flex">
@@ -39,7 +94,7 @@ const HomeComponent = () => {
         </div>
       </div>
 
-      <div className="h-full w-4/5 bg-app-lightblack">
+      <div className="h-full w-4/5 bg-app-lightblack overflow-auto">
         <div className="navbar w-full h-1/10 app-lightblack flex items-center justify-end">
           <div className="w-1/2 flex h-full">
             <div className="w-3/5 flex justify-around h-full items-center">
@@ -66,54 +121,75 @@ const HomeComponent = () => {
             </div>
           </div>
         </div>
-        <div className="content p-10">
-          <PlaylistView/>
+        <div className="content p-8 pt-0 overflow-auto">
+          <PlaylistView 
+            titleText="Focus"
+            cardsData={focusCardsData}
+            />
+          <PlaylistView 
+            titleText="Spotify Playlists"
+            cardsData={spotifyPlaylistsCardData}
+            />
+          <PlaylistView 
+          titleText="Sound Of India"
+          cardsData={focusCardsData}
+          />
         </div>
       </div>
     </div>
   );
 };
 
-const PlaylistView = () =>{
-   return <div className="text-white">
-    <div className="text-2xl font-semibold mb-5">Focus</div>
-    <div className="w-full font-semibold flex justify-between ">
-      <Card
+const PlaylistView = ({titleText, cardsData}) =>{
+   return <div className="text-white mt-8">
+    <div className="text-2xl font-semibold mb-5">{titleText}</div>
+    <div className="w-full flex justify-between space-x-4">
+      {
+        cardsData.map((item) => {
+          return <Card title={item.title} description={item.description} imgUrl={item.imgUrl}/>
+        }) 
+      }
+      {/* <Card
         title= "Naruto"
         description= "All the songs you know from Naruto and Naruto Shippuden."
+        imgUrl="https://miro.medium.com/v2/resize:fit:5120/1*42ebJizcUtZBNIZPmmMZ5Q.jpeg"
       />
       <Card
         title= "Naruto"
         description= "All the songs you know from Naruto and Naruto Shippuden."
+        imgUrl="https://miro.medium.com/v2/resize:fit:5120/1*42ebJizcUtZBNIZPmmMZ5Q.jpeg"
       />
       <Card
         title= "Naruto"
         description= "All the songs you know from Naruto and Naruto Shippuden."
+        imgUrl="https://miro.medium.com/v2/resize:fit:5120/1*42ebJizcUtZBNIZPmmMZ5Q.jpeg"
       />
       <Card
         title= "Naruto"
         description= "All the songs you know from Naruto and Naruto Shippuden."
+        imgUrl="https://miro.medium.com/v2/resize:fit:5120/1*42ebJizcUtZBNIZPmmMZ5Q.jpeg"
       />
       <Card
         title= "Naruto"
         description= "All the songs you know from Naruto and Naruto Shippuden."
-      />
+        imgUrl="https://miro.medium.com/v2/resize:fit:5120/1*42ebJizcUtZBNIZPmmMZ5Q.jpeg"
+      /> */}
     </div>
    </div>
 }
 
-const Card = ({title, description}) =>{
+const Card = ({title, description, imgUrl}) =>{
   return(
-    <div className="bg-black bg-opacity-60 rounded-lg w-1/6 p-4">
+    <div className="bg-black bg-opacity-10 rounded-lg w-1/6 p-4">
       <div className="pb-4 pt-2">
         <img 
-          className="w-full rounded-md" 
-          src="https://miro.medium.com/v2/resize:fit:5120/1*42ebJizcUtZBNIZPmmMZ5Q.jpeg"
+          className="w-full rounded-md " 
+          src={imgUrl}
           alt="label"
           />
         
       </div>
-      <div className="text-white text-sm">{title}</div>
+      <div className="text-white py-3">{title}</div>
       <div className="text-gray-500 text-sm">{description}</div>
     </div>
   )
